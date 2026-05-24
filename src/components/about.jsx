ffbec1ee91fae69ff7241407dbbd6fec14a653d3
Daphrose <daphrose405@gmail.com>
2026-05-24 16:36:52 +0300

    updated my about me page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,8 +17,11 @@ function About() {
           <h2>Who I Am</h2>
 
           <p>
-            I'm Daphrose, an aspiring Data Analyst and AI Analytics Enthusiast
-           a Computer Science finalist from Chuka University currently awaiting graduation.
+                I am Obuya  Daphrose, a Computer Science finalist from Chuka University
+            with a growing passion for Data Analytics and business intelligence.
+            I enjoy working with data to uncover patterns, generate insights,
+            and build solutions that help transform raw information into
+            meaningful decision-making tools.
           </p>
 
         </div>
@@ -28,9 +31,12 @@ function About() {
           <h2> What I Do</h2>
 
           <p>
-            I enjoy transforming raw data into meaningful insights through
-            interactive dashboards, business intelligence solutions,
-            and modern analytics applications.
+             My analytics journey began through exploring data visualization,
+            dashboard development, and sales analysis projects.
+            Over time, I have gained experience in data cleaning,
+            KPI generation, exploratory data analysis (EDA),
+            and interactive dashboard creation using tools like
+            Python, Pandas, Plotly, Excel, Streamlit, and SQL.
           </p>
 
         </div>
